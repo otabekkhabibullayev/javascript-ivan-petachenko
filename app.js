8dbf25bbbeda86bf4d6cabc4968357391c972a0c
Otabek <otashkin25@gmail.com>
2022-08-30 11:48:38 +0500

    condition and cycle

--- app.js
+++ app.js
@@ -10,13 +10,33 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмоиренных фильмов?', '')
-const b = prompt('На сколько оцените его?', '')
-const c = prompt('Один из последних просмоиренных фильмов?', '')
-const d = prompt('На сколько оцените его?', '')
+if (personalMovieDB.count < 10) {
+    console.log("Просмотренно давольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+    console.log('вы классный зритель');
+} else if (personalMovieDB.count >= 50) {
+    console.log('вы киноман');
+} else {
+    console.log('подошла ошибка');
+}
 
-personalMovieDB.movies[a] = b
-personalMovieDB.movies[c] = d
+
+
+
+for (let index = 0; index < 2; index++) {
+    const a = prompt('Один из последних просмоиренных фильмов?', '')
+    const b = prompt('На сколько оцените его?', '')
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b
+        console.log('done');
+    } else {
+        i--
+    }
+
+
+
+}
 
 
 console.log(personalMovieDB);
